f72afc4010501a33f2e273ad3b893155cf9cd30d
Masatoshi Tada <rosso.e.nero84@gmail.com>
2020-03-17 18:10:05 +0900

    first commit

---
+++ 03-ingress/google-kubernetes-engine.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+kubectl create clusterrolebinding cluster-admin-binding --clusterrole cluster-admin --user $(gcloud config get-value account)
+kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/master/deploy/static/mandatory.yaml
+kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/master/deploy/static/provider/cloud-generic.yaml
